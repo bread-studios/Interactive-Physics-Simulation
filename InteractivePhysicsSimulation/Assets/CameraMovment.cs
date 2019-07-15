f069b8640d280a4fea5fc758caea6aadf0a2ddf6
muyangyan1 <50777955+muyangyan1@users.noreply.github.com>
2019-07-15 12:22:03 -0700

    stuff

---
+++ InteractivePhysicsSimulation/Assets/CameraMovment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour {
+
+    //Mouse direction
+    private Vector2 mD;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        Vector2 mC = new Vector2 (Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+    }
+}
