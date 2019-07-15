163a2fad3b478b3f8e272624d8fc7f7f70f6ce07
GreggZumbari <greggory.png@gmail.com>
2019-07-15 14:50:16 -0700

    Yee haw

---
+++ InteractivePhysicsSimulation/Assets/Scripts/RigidBodyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	//If press space, return velocity in console
+	void Update () {
+        if (Input.GetKey("space"))
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY
+            | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | 
+            RigidbodyConstraints.FreezePositionZ;
+        }
+	}
+
+    public float getVelocity()
+    {
+        return 1f;
+    }
+
+    /*public float getAcceleration()
+    {
+        
+    }*/
+}
