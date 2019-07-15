a5f714bf213363d29c25fc04970947e77d666e17
Muyang Yan <muyangyan.me@gmail.com>
2019-07-15 14:01:36 -0700

    PlayManager Script added

---
+++ InteractivePhysicsSimulation/Assets/Scripts/PlayingManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingManager : MonoBehaviour {
+
+    public bool IsPlaying;
+
+    public void TogglePlayPause()
+    {
+        IsPlaying = !IsPlaying;
+    }
+}
