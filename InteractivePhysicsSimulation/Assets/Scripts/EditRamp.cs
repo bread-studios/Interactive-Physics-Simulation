638c310b8cd335be56a00de6ea95e5b440a39774
Muyang Yan <muyangyan.me@gmail.com>
2019-07-16 11:20:40 -0700

    The ramp works

--- InteractivePhysicsSimulation/Assets/Scripts/EditRamp.cs
+++ InteractivePhysicsSimulation/Assets/Scripts/EditRamp.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EditRamp : MonoBehaviour
 {
 
-    public float angle = 45.0f;
+    public float angle;
+    private float trueAngle;
     private float xScale;
     private float yScale;
     private float zScale;
@@ -13,14 +14,19 @@ public class EditRamp : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (angle <= 0 || angle >= 90)
+        if (angle <= 0)
         {
-            angle = 45;
+            angle = 0.000000001f;
         }
+        if (angle >= 90)
+        {
+            angle = 90;
+        }
+        trueAngle = angle * Mathf.Deg2Rad;
         yScale = transform.localScale.y;
         xScale = transform.localScale.x;
-        zScale = Mathf.Rad2Deg * Mathf.Atan(angle * Mathf.Deg2Rad * Mathf.Deg2Rad) * yScale;
+        zScale = Mathf.Tan(trueAngle) * yScale;
         transform.localScale = new Vector3(xScale, yScale, zScale);
-        Debug.Log(xScale+","+yScale+","+zScale);
+        transform.position = new Vector3(transform.position.x, (zScale/100) , transform.position.z);
     }
 }
