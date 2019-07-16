fed95221ce786dc7261b78e5ad8e5ab2be1a3d1b
David Zager <walkdownlane22@gmail.com>
2019-07-16 14:04:57 -0700

    Button Changes Sprite

---
+++ InteractivePhysicsSimulation/Assets/ChangeImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour {
+
+    public Sprite start;
+    public Sprite stop;
+    Image image;
+
+    public void Start()
+    {
+        image = GetComponent<Image>();
+        image.sprite = stop;
+    }
+
+    public void changeSprite()
+    {
+        if (image.sprite.name.Equals("StopButton"))
+        {
+            image.sprite = start;
+        }
+        else if (image.sprite.name.Equals("StartButton"))
+        {
+            image.sprite = stop;
+        }
+    }
+}
---
+++ InteractivePhysicsSimulation/Assets/ObjectSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+    public Transform spawn;
+    private ArrayList objects;
+
+    private void Start()
+    {
+        objects = new ArrayList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            StartCoroutine("SpawnBlock");
+        }
+    }
+
+    IEnumerator SpawnBlock()
+    {
+        Instantiate(prefab, spawn);
+        yield return new WaitForSeconds(1);
+    }
+}
