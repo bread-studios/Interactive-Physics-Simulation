bde59e1cf2e1d0ea0b899208fe4e2f14077c9346
Muyang Yan <muyangyan.me@gmail.com>
2019-07-16 11:24:13 -0700

    Update EditRamp.cs

--- InteractivePhysicsSimulation/Assets/Scripts/EditRamp.cs
+++ InteractivePhysicsSimulation/Assets/Scripts/EditRamp.cs
@@ -27,10 +27,6 @@ public class EditRamp : MonoBehaviour
         xScale = transform.localScale.x;
         zScale = Mathf.Tan(trueAngle) * yScale;
         transform.localScale = new Vector3(xScale, yScale, zScale);
-<<<<<<< HEAD
         transform.position = new Vector3(transform.position.x, (zScale/100) , transform.position.z);
-=======
-        //Debug.Log(xScale+","+yScale+","+zScale);
->>>>>>> 256de74736bc6571eb78027df7894bf616c6ff45
     }
 }
