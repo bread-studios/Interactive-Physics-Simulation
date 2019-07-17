cb1a7b22ed2cf94a62b3ff7f2bedd7edca195fe8
GreggZumbari <greggory.png@gmail.com>
2019-07-17 14:03:02 -0700

    g

---
+++ InteractivePhysicsSimulation/Assets/Scripts/DrawGrid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGrid : MonoBehaviour {
+
+    Vector3 dim;
+    float len;
+    int gridSize;
+
+	// Draw the grid on the ground at the start
+	void Start () {
+
+        dim = GetComponent<Renderer>().bounds.size;
+        len = dim.x;
+        gridSize = 1000;
+
+        for (int i = 0; i < 100; i++)
+        {
+            Debug.DrawLine(new Vector3((len*i)/gridSize, 0f, len/2), new Vector3(0, 0, 0), Color.red);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
