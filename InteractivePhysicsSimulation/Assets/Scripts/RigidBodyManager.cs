03bec60c50de721e917a7f2698a7b3fae1008b1d
Muyang Yan <muyangyan.me@gmail.com>
2019-07-23 17:59:34 -0700

    Revert "friction, bounciness, toggle (almost)"
    This reverts commit 5da981a72723fa241be4d91feaf44677917807b9.

--- InteractivePhysicsSimulation/Assets/Scripts/RigidBodyManager.cs
+++ InteractivePhysicsSimulation/Assets/Scripts/RigidBodyManager.cs
@@ -7,7 +7,6 @@ public class RigidBodyManager : MonoBehaviour
     private GameObject Manager;
     private PlayingManager pm;
     private bool IsPlayingDelayed;
-    private SelectedObjectControllerAndSelecter socas;
     public float mass;
     public Vector3 vel;
     public Vector3 rot;
@@ -76,8 +75,9 @@ public class RigidBodyManager : MonoBehaviour
                 | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY |
                 RigidbodyConstraints.FreezePositionZ);
                 r.velocity = vel/100;
-                r.angularVelocity = rot/100;
+                r.angularVelocity = rot/100 ;
                 r.mass = mass;
+
             }
         }
         else
