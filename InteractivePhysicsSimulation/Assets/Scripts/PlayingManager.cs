f27f5da2f7c2fd506f38b4ec7ab7f865219693f1
GreggZumbari <greggory.png@gmail.com>
2019-07-24 11:12:04 -0700

    YEE HAW GLOBAL SETTIONGS

--- InteractivePhysicsSimulation/Assets/Scripts/PlayingManager.cs
+++ InteractivePhysicsSimulation/Assets/Scripts/PlayingManager.cs
@@ -6,19 +6,26 @@ public class PlayingManager : MonoBehaviour {
 
     public GameObject Selected;
     public GameObject PropertiesPanel;
+    public GameObject GlobOptionPanel;
     public SelectedObjectControllerAndSelecter socas;
     private void Start()
     {
         PropertiesPanel.SetActive(true);
+        GlobOptionPanel.SetActive(false);
     }
 
     public bool IsPlaying = false;
+    public bool GSPIsEnabled = false;
 
     private void Update()
     {
         if (Input.GetKeyUp("p"))
         {
             TogglePlayPause();
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            ToggleGlobalSettingsWindow();
         }
         if(Selected == null)
         {
@@ -34,11 +41,20 @@ public class PlayingManager : MonoBehaviour {
     public void TogglePlayPause()
     {
         Debug.Log("toggled play/pause");
-        IsPlaying = !IsPlaying; 
+        IsPlaying = !IsPlaying;
     }
     public void ReactToPropertyChange()
     {
         socas = Selected.GetComponent<SelectedObjectControllerAndSelecter>();
         socas.KidsReactToPropertyDamage();
+    }
+    public void ToggleGlobalSettingsWindow() //Frick you muyang i'm doing it in this class
+    {
+        GSPIsEnabled = !GSPIsEnabled;
+        GlobOptionPanel.SetActive(GSPIsEnabled);
+    }
+    public void TogglePlayButtonIcon()
+    {
+        //change icon
     }
 }
