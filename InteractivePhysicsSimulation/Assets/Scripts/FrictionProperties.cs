e684523e7ab94fb55cafca590692bec93b96cb49
Muyang Yan <muyangyan.me@gmail.com>
2019-07-23 17:59:57 -0700

    Revert "Revert "friction, bounciness, toggle (almost)""
    This reverts commit 03bec60c50de721e917a7f2698a7b3fae1008b1d.

---
+++ InteractivePhysicsSimulation/Assets/Scripts/FrictionProperties.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrictionProperties : MonoBehaviour
+{
+
+    private Material Frictionless;
+    private PhysicMaterial ChangeMaterial;
+    public Collider Butter;
+
+    // Use this for initialization
+    void Start()
+    {
+        Butter = GetComponent<Collider>();
+        Butter.material.dynamicFriction = 1f;
+        Butter.material.staticFriction = 1f;
+        Butter.material.bounciness = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // (Adjust later) If the object is on selection and 'F' is pressed, the object will turn frictionless
+        //Friction
+        if (Input.GetKeyDown(KeyCode.F) == true)
+        {
+
+            Butter.material.dynamicFriction = 0f;
+            Butter.material.staticFriction = 0f;
+        }
+        else if (Input.GetKeyUp(KeyCode.F) == true)
+        {
+            Butter.material.dynamicFriction = 1f;
+            Butter.material.staticFriction = 1f;
+        }
+
+        // Bounciness
+        if (Input.GetKeyDown(KeyCode.B) == true)
+        {
+            Butter.material.bounciness = 1f;
+        }
+        else if (Input.GetKeyUp(KeyCode.B) == true)
+        {
+            Butter.material.bounciness = 0f;
+        }
+
+    }
+}
+
+
