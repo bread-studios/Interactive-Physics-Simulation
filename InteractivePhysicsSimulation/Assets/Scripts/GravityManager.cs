bf85067d04a1df0a56bd9d322573fc606116a001
Muyang Yan <muyangyan.me@gmail.com>
2019-07-25 14:25:31 -0700

    Revert "Merge branch 'bread-time' of https://github.com/bread-studios/Interactive-Physics-Simulation into bread-time"
    This reverts commit 23e64e2887b0340840ddc3344f092fee8a74e04a, reversing
    changes made to ea8bf3aee3523b639ff92525356798e6347c8485.

--- InteractivePhysicsSimulation/Assets/Scripts/GravityManager.cs
+++ InteractivePhysicsSimulation/Assets/Scripts/GravityManager.cs
@@ -1,75 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GravityManager : MonoBehaviour {
 
     public Vector3 direction;
     Vector3 currentDirection;
 
-    public GameObject gravX;
-    public GameObject gravY;
-    public GameObject gravZ;
-
 	//Set gravity to be regular earth gravity
 	void Start () {
         Physics.gravity = new Vector3(0, -9.81f, 0);
-        direction = enforceUpperLimit(10000, Physics.gravity);
-        AntiGravityKidsReactToPropertyDamage();
-    }
+        direction = Physics.gravity;
+	}
 	
+	//
 	void Update () {
-        enforceUpperLimit(10000, direction);
+        enforceUpperLimit(10000);
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            invertGravity('a');
+        }
 		if (currentDirection != direction)
         {
             changeGravity(direction);
         }
         currentDirection = Physics.gravity;
         direction = Physics.gravity;
-        AntiGravityParentsReactToKidDamage();
-        AntiGravityKidsReactToPropertyDamage();
-        if (Input.GetKeyUp(KeyCode.G))
-        {
-            invertGravity('a');
-        }
-    }
-
-    //Update the text fields to show the current gravity vector
-    void AntiGravityKidsReactToPropertyDamage()
-    {
-        gravX.GetComponent<InputField>().text = direction.x.ToString();
-        gravY.GetComponent<InputField>().text = direction.y.ToString();
-        gravZ.GetComponent<InputField>().text = direction.z.ToString();
-    }
-
-    //Update the gravity vector to be what the text fields show
-    void AntiGravityParentsReactToKidDamage()
-    {
-        direction = enforceUpperLimit(10000, new Vector3(float.Parse(gravX.GetComponent<InputField>().text), 
-            float.Parse(gravY.GetComponent<InputField>().text), 
-            float.Parse(gravZ.GetComponent<InputField>().text)));
-        /*direction.x = float.Parse(gravX.GetComponent<InputField>().text);
-        direction.y = float.Parse(gravY.GetComponent<InputField>().text);
-        direction.z = float.Parse(gravZ.GetComponent<InputField>().text);*/
     }
 
     //If the gravity gets too high or low herobrine will haunt your game
-    Vector3 enforceUpperLimit(int upperLimit, Vector3 direction)
+    void enforceUpperLimit(int upperLimit)
     {
         if (direction.x > upperLimit)
+        {
             direction.x = upperLimit;
+        }
         if (direction.x < -upperLimit)
+        {
             direction.x = -upperLimit;
+        }
         if (direction.y > upperLimit)
+        {
             direction.y = upperLimit;
+        }
         if (direction.y < -upperLimit)
+        {
             direction.y = -upperLimit;
+        }
         if (direction.z > upperLimit)
+        {
             direction.z = upperLimit;
+        }
         if (direction.z < -upperLimit)
+        {
             direction.z = -upperLimit;
-        return direction;
+        }
     }
 
     //Change gravity to a custom direction
@@ -83,13 +68,21 @@ public class GravityManager : MonoBehaviour {
     {
         Vector3 g = Physics.gravity;
         if (direction == 'x')
-            g.x = -g.x;
+        {
+            g.x = -g.x;    
+        }
         if (direction == 'y')
+        {
             g.y = -g.y;
+        }
         if (direction == 'z')
+        {
             g.z = -g.z;
+        }
         if (direction == 'a')
+        {
             g = -g;
+        }
         Physics.gravity = g;
     }
 
--- InteractivePhysicsSimulation/Assets/Scripts/PlayingManager.cs
+++ InteractivePhysicsSimulation/Assets/Scripts/PlayingManager.cs
@@ -7,33 +7,23 @@ public class PlayingManager : MonoBehaviour
 
     public GameObject Selected;
     public GameObject PropertiesPanel;
-    public GameObject GlobOptionPanel;
     public SelectedObjectControllerAndSelecter socas;
     public RigidBodyManager rbm;
     public bool isSliderChanged;
     private void Start()
     {
         PropertiesPanel.SetActive(true);
-        GlobOptionPanel.SetActive(false);
     }
 
     public bool IsPlaying = false;
-    public bool GSPIsEnabled = false;
 
     private void Update()
     {
-        //Toggle playing or paused
         if (Input.GetKeyUp("p"))
         {
             TogglePlayPause();
         }
-        //Toggle the appearance of the global settings panel
-        if (Input.GetKeyUp(KeyCode.Q)) 
-        {
-            ToggleGlobalSettingsWindow();
-        }
-        //If nothing selected, don't show the properties panel
-        if(Selected == null)
+        if (Selected == null)
         {
             PropertiesPanel.SetActive(false);
         }
@@ -54,6 +44,7 @@ public class PlayingManager : MonoBehaviour
         socas = Selected.GetComponent<SelectedObjectControllerAndSelecter>();
         socas.KidsReactToPropertyDamage();
     }
+
     public void ToggleStatic()
     {
         Debug.Log("ToggleStatic");
@@ -70,11 +61,5 @@ public class PlayingManager : MonoBehaviour
     public void NotSliderChanged()
     {
         isSliderChanged = false;
-    }
-}
-    public void ToggleGlobalSettingsWindow() //Frick you muyang i'm doing it in this class
-    {
-        GSPIsEnabled = !GSPIsEnabled;
-        GlobOptionPanel.SetActive(GSPIsEnabled);
     }
 }
