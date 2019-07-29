6f6a8365903b8eb9de653c2084ea57e901da8a2c
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-07-29 09:16:00 -0700

    touchthecow
    touch it

---
+++ InteractivePhysicsSimulation/Assets/Scripts/ResetButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetButton : MonoBehaviour {
+
+    public Button pressButton;
+    public GameObject thingy;
+    public InputField posi, rotate, scal, velo, angvelo;
+
+    // Use this for initialization
+    void Start () {
+        thingy = FindObjectOfType<GameObject>();
+        posi.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        rotate.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float posiX;
+        float posiY;
+        float posiZ;
+
+        if (pressButton == true)
+        {
+                        
+        }
+	}
+}
