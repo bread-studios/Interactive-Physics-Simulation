73d60b68ebf91763e0e54fdd57a92b0ee5dc3537
GreggZumbari <greggory.png@gmail.com>
2019-07-29 11:35:40 -0700

    tgifridays

---
+++ InteractivePhysicsSimulation/Assets/Scripts/GlobalSettingsManager.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * This class is where all global variables will be managed and changed
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalSettingsManager : MonoBehaviour {
+
+    private GameObject m;
+    private GameObject GridMaker;
+    private GameObject gridToggle;
+    
+
+	//Initialize all GameObjects
+	void Start () {
+        m = GameObject.FindWithTag("Manager");
+        GridMaker = GameObject.FindWithTag("GridMaker");
+        gridToggle = GameObject.Find("GridCB");
+	}
+	
+	void Update () {
+		if (gridToggle.GetComponent<Toggle>().isOn)
+        {
+            GridMaker.SetActive(true);
+        }
+        else
+        {
+            GridMaker.SetActive(false);
+        }
+	}
+}
