2e3bb0ead3919940554ffed1874cc98ff67e1521
GreggZumbari <greggory.png@gmail.com>
2019-08-01 12:51:55 -0700

    sup gamers

--- InteractivePhysicsSimulation/Assets/Scripts/RainbowGrid.cs
+++ InteractivePhysicsSimulation/Assets/Scripts/RainbowGrid.cs
@@ -1,16 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/**
+ * Whatever this class is attached to will be rainbowy
+ */
 using UnityEngine;
+using System.Collections;
 
-public class RainbowGrid : MonoBehaviour {
+public class RainbowGrid : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool gay;
+
+    Color[] colorList; //These will be the colors that will be switched between in this order
+    Renderer r;
+    float transitionTime = 4f;
+    int iterator;
+
+    void Start()
+    {
+        gay = true;
+        r = GetComponent<Renderer>();
+        colorList = new Color[6];
+        colorList[0] = Color.red;
+        colorList[1] = new Color(1f, 0.46f, 0.08f, 1f); //Orange
+        colorList[2] = Color.yellow;
+        colorList[3] = Color.green;
+        colorList[4] = Color.blue;
+        colorList[5] = Color.magenta;
+
+        if (gay == true)
+            StartCoroutine(RainbowColor());
+    }
+
+    IEnumerator RainbowColor()
+
+    {
+        while (true)
+        {
+            float transitionRate = 0f;
+            r.material.SetColor("_Color", Color.Lerp(r.material.color, colorList[iterator], Time.deltaTime * transitionRate));
+            r.material.SetColor("_EmmisionColor", Color.Lerp(r.material.color, colorList[iterator], Time.deltaTime * transitionRate));
+            transitionRate += Time.deltaTime / transitionTime;
+            iterator++;
+            if (iterator >= 5)
+                iterator = 0;
+        }
+    }
+
 }
